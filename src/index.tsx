24515900195fd14c07c02b60188069c55f8b12d9
Gabriel Oliveira <gabrieloliveira472@gmail.com>
2022-09-15 15:16:23 -0300

    test

--- src/index.tsx
+++ src/index.tsx
@@ -1,81 +1,18 @@
-import "./index.scss";
-import { createRoot } from "react-dom/client";
-import { StrictMode } from "react";
-import { App } from "./App";
-import {
-  KcApp as KcAppBase,
-  defaultKcProps,
-  getKcContext,
-  kcMessages,
-  useDownloadTerms
-} from "keycloakify";
-import { useCssAndCx } from "tss-react";
-import tos_en_url from "./tos_en.md";
-import tos_fr_url from "./tos_fr.md";
-import "./kcMessagesExtension"
+import { createRoot } from "react-dom/client"
+import { StrictMode, lazy, Suspense } from "react"
+import { kcContext } from "./KcApp/kcContext"
 
-const { kcContext } = getKcContext({
-  /* Uncomment to test th<e login page for development */
-  //"mockPageId": "login.ftl",
-  "mockData": [
-    {
-      "pageId": "login.ftl",
-      "locale": {
-        "currentLanguageTag": "fr" //When we test the login page we do it in french
-      }
-    }
-  ]
-});
+const App = lazy(() => import("./App"))
+const KcApp = lazy(() => import("./KcApp"))
 
 if (kcContext !== undefined) {
-  console.log(kcContext);
+  console.log(kcContext)
 }
-
-function KcApp() {
-
-  if (kcContext === undefined) {
-    throw new Error();
-  }
-
-  useDownloadTerms({
-    kcContext,
-    "downloadTermMarkdown": async ({ currentKcLanguageTag }) => {
-
-      kcMessages[currentKcLanguageTag].termsTitle = "";
-
-      const markdownString = await fetch((() => {
-        switch (currentKcLanguageTag) {
-          case "fr": return tos_fr_url;
-          default: return tos_en_url;
-        }
-      })())
-        .then(response => response.text());
-
-      return markdownString;
-
-    }
-  });
-
-  const { css } = useCssAndCx();
-
-  return (
-    <KcAppBase
-      kcContext={kcContext}
-      {...{
-        ...defaultKcProps,
-        "kcHeaderWrapperClass": css({ "color": "red", "fontFamily": '"Work Sans"' })
-      }}
-    />
-  );
-}
-
-
+console.log(kcContext)
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {
-      kcContext === undefined ?
-        <App /> :
-        <KcApp />
-    }
+    <Suspense>
+      {kcContext === undefined ? <App /> : <KcApp kcContext={kcContext} />}
+    </Suspense>
   </StrictMode>
-);
+)
